d49cc52a9d74064bdb66e2f8a9204e1a841840f6
Manuel Gauto <Manuel Gauto@ManuelGauto-PC>
2011-12-26 21:13:07 -0500

    Adjust Dependency Version

--- src/main/java/Startup/NewMain.java
+++ src/main/java/Startup/NewMain.java
@@ -16,8 +16,9 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        FacebookInterface fb=new FacebookInterface("AAACx4m11Mv4BAOlxeJECgGNcVjHlaVr2PCU7i5iNZC0jdDCeZBNhPRYPbrLAn7X8R3EJz7H73MZCpP9qn3Gvjsif5SXe8UZD");
-        fb.whoami();
-        fb.PostStatus("ew PVI");
+        //FacebookInterface fb=new FacebookInterface("AAACx4m11Mv4BAOlxeJECgGNcVjHlaVr2PCU7i5iNZC0jdDCeZBNhPRYPbrLAn7X8R3EJz7H73MZCpP9qn3Gvjsif5SXe8UZD");
+        //fb.whoami();
+        //fb.PostStatus("ew PVI");
+        
     }
 }
